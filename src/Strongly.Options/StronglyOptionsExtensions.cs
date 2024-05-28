e82937e6265100cbae5ac694f95e34f56ebcaa59
nikitakaralyus <nikitakaralyus@gmail.com>
2024-05-28 20:11:05 +0300

    Added xml doc

--- src/Strongly.Options/StronglyOptionsExtensions.cs
+++ src/Strongly.Options/StronglyOptionsExtensions.cs
@@ -10,11 +10,35 @@ public static class StronglyOptionsExtensions
         IServiceCollection services,
         IConfiguration configuration);
 
+    /// <summary>
+    /// Scans the assembly of the specified type <typeparamref name="T"/> and registers all options types
+    /// marked with the <see cref="StronglyOptionsAttribute"/>.
+    /// </summary>
+    /// <typeparam name="T">The type whose assembly should be scanned for options types.</typeparam>
+    /// <example>
+    /// <code>
+    /// var configuration = builder.Configuration;
+    /// builder.Services.AddStronglyOptions&lt;Program&gt;(configuration);
+    /// </code>
+    /// </example>
     public static IServiceCollection AddStronglyOptionsFromAssemblyContainingType<T>(
         this IServiceCollection services,
         IConfiguration configuration) =>
         services.AddStronglyOptions(configuration, typeof(T).Assembly);
 
+    /// <summary>
+    /// Scans the specified assembly and registers all options types marked with the <see cref="StronglyOptionsAttribute"/>.
+    /// </summary>
+    /// <param name="assembly">
+    /// The assembly containing the options types. If not specified or null, uses the assembly of the calling method: <br />
+    /// <c>Assembly.GetCallingAssembly()</c>
+    /// </param>
+    /// <example>
+    /// <code>
+    /// var configuration = builder.Configuration;
+    /// builder.Services.AddStronglyOptions(configuration);
+    /// </code>
+    /// </example>
     public static IServiceCollection AddStronglyOptions(
         this IServiceCollection services,
         IConfiguration configuration,
