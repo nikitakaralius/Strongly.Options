eb5552d17ee309273f73166a247fb4b062458b0e
nikitakaralyus <nikitakaralyus@gmail.com>
2024-06-02 15:54:50 +0300

    Made SectionNotFoundException public but it's constructor internal

--- src/Strongly.Options/SectionNotFoundException.cs
+++ src/Strongly.Options/SectionNotFoundException.cs
@@ -1,6 +1,6 @@
 namespace Strongly.Options;
 
-internal sealed class SectionNotFoundException : Exception
+public sealed class SectionNotFoundException : Exception
 {
-    public SectionNotFoundException(string message) : base(message) { }
+    internal SectionNotFoundException(string message) : base(message) { }
 }
