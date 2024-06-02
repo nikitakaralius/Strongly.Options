9720907235cdac324c63da550b041733bf11fdee
nikitakaralius <nikitakaralyus@gmail.com>
2024-06-02 18:54:00 +0300

    Added summaries to test data

--- tests/Strongly.Options.Tests/TestData/ApplicationOptions.cs
+++ tests/Strongly.Options.Tests/TestData/ApplicationOptions.cs
@@ -2,6 +2,9 @@ using Microsoft.Extensions.Configuration;
 
 namespace Strongly.Options.Tests.TestData;
 
+/// <summary>
+/// This type is being used in <see cref="StronglyOptionsExtensionsTests"/> tests. Do not change anything here.
+/// </summary>
 [StronglyOptions("App")]
 public sealed record ApplicationOptions
 {
--- tests/Strongly.Options.Tests/TestData/FeatureOptions.cs
+++ tests/Strongly.Options.Tests/TestData/FeatureOptions.cs
@@ -1,5 +1,8 @@
 namespace Strongly.Options.Tests.TestData;
 
+/// <summary>
+/// This type is being used in <see cref="StronglyOptionsExtensionsTests"/> tests. Do not change anything here.
+/// </summary>
 [StronglyOptions(StronglyOptionsSection.Root)]
 public sealed record FeatureOptions
 {
