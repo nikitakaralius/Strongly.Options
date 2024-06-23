0b90d1d18f458c1c9e8b41d083d9a0fe01a0f1ca
nikitakaralius <nikitakaralyus@gmail.com>
2024-06-23 18:01:58 +0300

    Implemented StronglyOptionsRegistrationSourceGenerator

---
+++ src/Strongly.Options.SourceGenerators/StronglyOptionsRegistrationSourceGenerator.cs
@@ -0,0 +1,104 @@
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Strongly.Options.SourceGenerators;
+
+using OptionsMetadata = (string FullyQualifiedTypeName, string Section);
+
+[Generator]
+public class StronglyOptionsRegistrationSourceGenerator : IIncrementalGenerator
+{
+    private const string StronglyOptionsNamespace = "Strongly.Options";
+
+    private const string StronglyOptionsAttribute = $"{StronglyOptionsNamespace}.StronglyOptionsAttribute";
+
+    private const string ConfigureTemplate =
+        """
+                    services.Configure<{Type}>(configuration.GetRequiredSection("{Section}"));
+        """;
+
+    private const string MethodTemplate =
+        $$"""
+        using Microsoft.Extensions.Configuration;
+        using Microsoft.Extensions.DependencyInjection;
+        
+        namespace {{StronglyOptionsNamespace}}
+        {
+            public static class StronglyOptionsServiceCollectionExtensions
+            {
+                public static IServiceCollection AddStronglyOptions(
+                    this IServiceCollection services,
+                    IConfiguration configuration)
+                {
+        {Configure}
+            
+                    return services;
+                }
+            }
+        }
+
+        """;
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var optionsMetadataProvider = context
+           .SyntaxProvider
+           .ForAttributeWithMetadataName(
+                StronglyOptionsAttribute,
+                predicate: (node, _) => node
+                    is ClassDeclarationSyntax
+                    or StructDeclarationSyntax
+                    or RecordDeclarationSyntax,
+                transform: GetOptionsMetadata)
+           .Collect();
+
+        context.RegisterSourceOutput(
+            optionsMetadataProvider,
+            GenerateCode);
+    }
+
+    private OptionsMetadata GetOptionsMetadata(
+        GeneratorAttributeSyntaxContext context,
+        CancellationToken _)
+    {
+        var optionsType = context
+           .TargetSymbol
+           .ToDisplayString();
+
+        var optionsAttributeData = context
+           .Attributes
+           .First(x => x.AttributeClass!.ToDisplayString() == StronglyOptionsAttribute);
+
+        var section = optionsAttributeData
+           .ConstructorArguments
+           .First()
+           .Value!
+           .ToString();
+
+        return ($"global::{optionsType}", section);
+    }
+
+    private void GenerateCode(
+        SourceProductionContext context,
+        ImmutableArray<OptionsMetadata> options)
+    {
+        var configureMethods = options
+           .Select(x => ConfigureTemplate
+               .Replace("{Type}", x.FullyQualifiedTypeName)
+               .Replace("{Section}", x.Section));
+
+        var mergedConfigureMethods = string.Join("\n", configureMethods);
+
+        var addStronglyOptionsExtensionMethod = MethodTemplate.Replace("{Configure}", mergedConfigureMethods);
+
+        context.AddSource(
+            "StronglyOptionsServiceCollectionExtensions.g.cs",
+            SourceText.From(addStronglyOptionsExtensionMethod, Encoding.UTF8));
+    }
+}
