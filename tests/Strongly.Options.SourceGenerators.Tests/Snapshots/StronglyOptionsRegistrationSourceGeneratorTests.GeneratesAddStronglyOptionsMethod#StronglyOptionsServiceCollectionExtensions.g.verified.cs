fd4f4e2fd3e85aa3f64c18590b63a2c61cc0b3ef
nikitakaralius <nikitakaralyus@gmail.com>
2024-06-23 18:15:49 +0300

    Ungitignored verified snapshot

---
+++ tests/Strongly.Options.SourceGenerators.Tests/Snapshots/StronglyOptionsRegistrationSourceGeneratorTests.GeneratesAddStronglyOptionsMethod#StronglyOptionsServiceCollectionExtensions.g.verified.cs
@@ -0,0 +1,28 @@
+﻿//HintName: StronglyOptionsServiceCollectionExtensions.g.cs
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Strongly.Options
+{
+    public static class StronglyOptionsServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Scans current assembly and registers all options types marked with the <see cref="StronglyOptionsAttribute"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// var configuration = builder.Configuration;
+        /// builder.Services.AddStronglyOptions(configuration);
+        /// </code>
+        /// </example>
+        public static IServiceCollection AddStronglyOptions(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<global::Company.Application.AuthOptions>(configuration.GetRequiredSection("Auth"));
+            services.Configure<global::ServiceOptions>(configuration.GetRequiredSection("Service"));
+    
+            return services;
+        }
+    }
+}
