35f6e793fa7d10d5733f80964e692df09ac4c03d
nikitakaralius <nikitakaralyus@gmail.com>
2024-06-26 21:31:52 +0300

    Separated Templates generation logic and constants from SourceGenerator. Made Assembly Name to be `moduleName` by default instead of empty string

--- src/Strongly.Options.SourceGenerators/StronglyOptionsRegistrationSourceGenerator.cs
+++ src/Strongly.Options.SourceGenerators/StronglyOptionsRegistrationSourceGenerator.cs
@@ -14,70 +14,12 @@ using OptionsMetadata = (string FullyQualifiedTypeName, string Section);
 [Generator]
 public class StronglyOptionsRegistrationSourceGenerator : IIncrementalGenerator
 {
-    private const string StronglyOptionsNamespace = "Strongly.Options";
-
-    private const string StronglyOptionsAttribute = $"{StronglyOptionsNamespace}.StronglyOptionsAttribute";
-    private const string StronglyOptionsModuleAttribute = $"{StronglyOptionsNamespace}.StronglyOptionsModuleAttribute";
-
-    private const string ConfigurationParameterName = "configuration";
-
-    private const string ConfigureTemplate =
-        """
-                    services.Configure<{Type}>(GetConfigurationSection("{Section}", configuration));
-        """;
-
-    private const string MethodTemplate =
-        $$"""
-        using Microsoft.Extensions.Configuration;
-        using Microsoft.Extensions.DependencyInjection;
-        
-        namespace {{StronglyOptionsNamespace}}
-        {
-            public static class StronglyOptionsServiceCollectionExtensions
-            {
-                /// <summary>
-                /// Scans current assembly and registers all options types marked with the <see cref="StronglyOptionsAttribute"/>.
-                /// </summary>
-                /// <example>
-                /// <code>
-                /// var configuration = builder.Configuration;
-                /// builder.Services.Add{ModuleName}StronglyOptions(configuration);
-                /// </code>
-                /// </example>
-                public static IServiceCollection Add{ModuleName}StronglyOptions(
-                    this IServiceCollection services,
-                    IConfiguration {{ConfigurationParameterName}})
-                {
-        {Configure}
-            
-                    return services;
-                }
-                
-                
-                private static IConfiguration GetConfigurationSection(string sectionName, IConfiguration configuration)
-                {
-                    if (sectionName == StronglyOptionsSection.Root)
-                        return configuration;
-                
-                    var section = configuration.GetSection(sectionName);
-                
-                    if (!section.AsEnumerable().Any(x => x.Value is not null))
-                        throw new SectionNotFoundException(
-                            $"Unable to find {section} section in Configuration");
-                
-                    return section;
-                }
-            }
-        }
-
-        """;
-
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
         var optionsMetadataProvider = context
            .SyntaxProvider
            .ForAttributeWithMetadataName(
-                StronglyOptionsAttribute,
+                WellKnownNamings.StronglyOptionsAttribute,
                 predicate: (node, _) => node
                     is ClassDeclarationSyntax
                     or RecordDeclarationSyntax,
@@ -89,8 +31,11 @@ public class StronglyOptionsRegistrationSourceGenerator : IIncrementalGenerator
            .Select((x, _) => x
                .Assembly
                .GetAttributes()
-               .FirstOrDefault(a => a.AttributeClass!.ToDisplayString() == StronglyOptionsModuleAttribute))
-           .Select((x, _) => x?.ConstructorArguments.First().Value?.ToString() ?? "");
+               .FirstOrDefault(a => a.AttributeClass!.ToDisplayString() == WellKnownNamings.StronglyOptionsModuleAttribute)?
+               .ConstructorArguments
+               .First()
+               .Value?
+               .ToString() ?? x.AssemblyName ?? "");
 
         context.RegisterSourceOutput(
             optionsMetadataProvider.Combine(moduleNameProvider),
@@ -107,7 +52,7 @@ public class StronglyOptionsRegistrationSourceGenerator : IIncrementalGenerator
 
         var optionsAttributeData = context
            .Attributes
-           .First(x => x.AttributeClass!.ToDisplayString() == StronglyOptionsAttribute);
+           .First(x => x.AttributeClass!.ToDisplayString() == WellKnownNamings.StronglyOptionsAttribute);
 
         var section = optionsAttributeData
            .ConstructorArguments
@@ -124,15 +69,17 @@ public class StronglyOptionsRegistrationSourceGenerator : IIncrementalGenerator
     {
         var configureMethods = sourceGenData
            .Options
-           .Select(x => ConfigureTemplate
-               .Replace("{Type}", x.FullyQualifiedTypeName)
-               .Replace("{Section}", x.Section));
+           .Select(x => Templates
+               .CreateConfigureMethodInvokeText(
+                    x.FullyQualifiedTypeName,
+                    x.Section));
 
         var mergedConfigureMethods = string.Join("\n", configureMethods);
 
-        var addStronglyOptionsExtensionMethod = MethodTemplate
-           .Replace("{ModuleName}", sourceGenData.ModuleName)
-           .Replace("{Configure}", mergedConfigureMethods);
+        var addStronglyOptionsExtensionMethod = Templates
+           .CreateDependencyInjectionExtensionText(
+                sourceGenData.ModuleName,
+                mergedConfigureMethods);
 
         context.AddSource(
             "StronglyOptionsServiceCollectionExtensions.g.cs",
